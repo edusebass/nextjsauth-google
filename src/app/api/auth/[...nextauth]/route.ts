0e0388a2fea0a4ac16c8de8088d5862e74bd6201
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2024-03-04 20:07:19 -0500

    commit de prueba

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google"
+import { SupabaseAdapter } from "@auth/supabase-adapter"
+import { Adapter } from "next-auth/adapters";
 // import CredentialsProvider from "next-auth/providers/credentials"   // its only if you want to use credentials provider
 
 
@@ -10,5 +12,12 @@ const handler = NextAuth({
             clientId: process.env.GOOGLE_CLIENT_ID ?? "",
             clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
         })
-    ]
+    ],
+    adapter: SupabaseAdapter({
+        url: process.env.NEXT_PUBLIC_SUPABASE_URL ?? "",
+        secret: process.env.SUPABASE_SERVICE_ROLE_KEY ?? "",
+        
+    }) as Adapter,
 })
+
+export {handler as GET, handler as POST}
